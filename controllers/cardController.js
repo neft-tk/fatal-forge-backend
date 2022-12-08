57f2b8b9252fe57a211f304fa6023d359c7421c0
Jonathan Cornet <cornetj2@gmail.com>
2022-12-08 10:24:05 -0800

    Cleaned up routes and such

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {User,Deck,Card} = require('../models');
+const { User, Deck, Card } = require('../models');
 
 router.get("/", (req,res) => {
     Card.findAll().then(cardData => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
-const {User,Deck,Card} = require('../models');
+const  {User, Deck, Card } = require('../models');
 
 router.get("/", (req,res)=> {
     User.findAll().then(userData => {
